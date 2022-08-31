3f987aae1f39a5cee6e4df2fdd4f9e93c3ddf762
pablo4192 <estanguet570@gmail.com>
2022-08-31 19:40:11 -0300

    subida ej2 creacion de componentes

---
+++ ejercicios/ejercicio2_Clase1/src/app/Entidades/usuario.ts
@@ -0,0 +1,6 @@
+export class Usuario {
+
+    nombre:string|undefined;
+    clave:string|undefined;
+
+}
---
+++ ejercicios/ejercicio2_Clase1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

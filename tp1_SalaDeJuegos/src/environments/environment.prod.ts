c6f843f49dadb43734ed53f04b2d03592e054eee
pablo4192 <estanguet570@gmail.com>
2023-03-16 22:31:50 -0300

    cambios en angular.json, enviroment.prod.ts

--- tp1_SalaDeJuegos/src/environments/environment.prod.ts
+++ tp1_SalaDeJuegos/src/environments/environment.prod.ts
@@ -9,5 +9,6 @@ export const environment = {
     authDomain: 'sala-de-juegos-c1619.firebaseapp.com',
     messagingSenderId: '720824693540',
   },
-  production: true
+  production: true,
+  apiPexels: '563492ad6f91700001000001b486ff9009234330a3818eb019b10e1b'
 };
